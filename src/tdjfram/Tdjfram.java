c21d4f7569ddd4e377e82a1714a02d7f8e1be742
sambou <sarrsambou03@gmail.com>
2024-01-07 17:54:49 +0000

    premier commit du projet java sans db

---
+++ src/tdjfram/Tdjfram.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tdjfram;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*import java.awt.Color;
+import javax.swing.*;
+import java.awt.event.*;*/
+/**
+ *
+ * @author sarrs
+ */
+public class Tdjfram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+       /*JFrame fen = new  JFrame();
+        fen.setSize(500,500);
+        
+        JPanel pan = new JPanel();
+        JLabel l =new JLabel("libelle");
+        l.setLayout(null);
+       // l.setBounds(10, 100, 025, 0100);
+        JTextField t =new JTextField(10);
+        fen.setContentPane(pan);
+        fen.setVisible(true);
+        JButton b =new JButton("ajouter");
+        fen.getContentPane().add(b);
+        fen.getContentPane().add(t);
+        fen.getContentPane().add(l);
+        //pan.setBackground(Color.black);
+        b.addActionListener((ActionEvent e) -> {
+            String nom =t.getText();
+            l.setText(nom);
+        });*/
+       
+       Frame f = new Frame();
+       f.setVisible(true) ;
+
+        
+      
+    }
+
+ 
+           
+}
